6c40fd7851432e4d3f34337e91e97ed87054a59e
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2026-02-16 10:13:29 -0500

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: 
-  readonly VITE_SUPABASE_SUPABASE_ANON_KEY: 
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_ANON_KEY: string
 }
 
 interface ImportMeta {
