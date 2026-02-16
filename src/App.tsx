bb81f87f46f6b34162c76605274de592ed524251
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2026-02-16 10:05:54 -0500

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { ProjectsPage } from './pages/ProjectsPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Header />
       <Routes>
         <Route path="/admin" element={<AdminPage />} />
