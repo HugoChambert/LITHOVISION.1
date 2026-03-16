6b88fb2015d6bbdb84df69db372785d4c9d598d3
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2026-03-16 09:24:23 -0700

    Add glassmorphism, theme toggle, lower nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext';
 import { Header } from './components/layout/Header';
 import { AdminPage } from './pages/AdminPage';
 import { VisualizePage } from './pages/VisualizePage';
@@ -6,14 +7,16 @@ import { ProjectsPage } from './pages/ProjectsPage';
 
 export default function App() {
   return (
-    <HashRouter>
-      <Header />
-      <Routes>
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/visualize" element={<VisualizePage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/" element={<Navigate to="/visualize" />} />
-      </Routes>
-    </HashRouter>
+    <ThemeProvider>
+      <HashRouter>
+        <Header />
+        <Routes>
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/visualize" element={<VisualizePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/" element={<Navigate to="/visualize" />} />
+        </Routes>
+      </HashRouter>
+    </ThemeProvider>
   );
 }
